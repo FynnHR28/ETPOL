03da1dc107670d3b9957c8cd9a78ea63c854852b
Fynn_rhr <136636012+FynnHR28@users.noreply.github.com>
2025-05-13 13:09:28 -0400

    comments

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import numpy as np
 
 if __name__ in '__main__':
     
+    # setting seed to ensure reproducability, without this its like shooting in the dark with no eyes ears or nose
     seed = 121
     random.seed(seed)  # Set Python's random seed
     np.random.seed(seed)  # Set NumPy's random seed
@@ -26,6 +27,7 @@ if __name__ in '__main__':
     
     """Model & training hyper parameters"""
     
+    # model configuration for any given run
     context_length = 512
     d_model = 768
     num_heads = 16
@@ -39,6 +41,7 @@ if __name__ in '__main__':
     pdrop = 0.3099236390195026
     batch_size = 32
     
+    # instantiate model with given parameters
     model = ETPOL(
         vocab_size=20000,
         context_length=context_length,
@@ -49,6 +52,7 @@ if __name__ in '__main__':
         num_encoders=num_encoders,
         pdrop=pdrop 
     )
+
 
     model_hyperparams = {
         'context_length': context_length,
@@ -63,12 +67,14 @@ if __name__ in '__main__':
         'num_epochs': num_epochs
     }
     
+    # helpful object to keep track of which hyperparameters were used for a given trial run
     model_hyperparams_str = '\n'.join(f'{k}: {v}' for k, v in model_hyperparams.items())
     print(model_hyperparams_str)
 
+    # make sure to make use of gpu if available
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     print(f"device: {device}")
-    # good for binary classification
+    # good for binary classification, label smoothing of 0.1
     loss_fn = nn.CrossEntropyLoss(label_smoothing=0.1)
 
     print('loading tokenizer')
@@ -77,20 +83,24 @@ if __name__ in '__main__':
     print('loading df..')
     df=pd.read_csv(Path('data/all_posts.csv'))
 
+    # splitting up the data, making sure to stratify by the label
     print('splitting into train and test sets')
     train_df, temp = train_test_split(df, test_size=0.3, stratify=df['affiliation'])
     val_df, test_df = train_test_split(temp, test_size=0.5, stratify=temp['affiliation'])
 
     print('creating datasets..')
+    # wrap df in dataset objects
     train_dataset = PostsDataset(df=train_df, tokenizer=tokenizer, context_length=context_length)
     val_dataset = PostsDataset(df=val_df, tokenizer=tokenizer, context_length=context_length)
     test_dataset = PostsDataset(test_df, tokenizer=tokenizer, context_length=context_length)
 
+    # dataloaders expect dataset objects, this class batches the observations and tokenizes them on the fly
     print('creating dataloaders')
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
     
+    # get training results, see test_utils for a description of this train method
     results = train(
         model=model,
         mod_name=mod_name,
@@ -108,4 +118,5 @@ if __name__ in '__main__':
 
     results = update_results(results, f1_test, acc_test, loss_test, prec_test, recall_test, 'test')
     
+    # save results (also in test_utils.py)
     save_results(results, model_hyperparams_str, mod_name)
