90e3080f63b55d7834432130f5576406d6012cbc
Fynn_rhr <136636012+FynnHR28@users.noreply.github.com>
2025-05-13 13:09:11 -0400

    mother of all commenting jobs is finished

--- annotate.py
+++ annotate.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import torch
 
 
+# the best configuration as discovered in opt.py
 context_length = 512
 d_model = 768
 num_heads = 16
@@ -12,7 +13,7 @@ num_hidden_layers = 4
 d_hidden = 2048
 num_encoders = 7 
  
- 
+# set the device to the gpu if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 lr =  6.871199163253075e-05
@@ -33,6 +34,7 @@ model = ETPOL(
 tokenizer = PreTrainedTokenizerFast.from_pretrained(Path('wp_tokenizer'), model_max_lenth=context_length)
 
 
+# load the saved state of the best ETPOL configuration, only executable in my remote connection to the hpc where model state is saved
 model.load_state_dict(torch.load('./models/etpol.pth'))
 print('model state loaded successfully')
 
@@ -40,6 +42,7 @@ print('model state loaded successfully')
 # same label map used during training
 reverse_label_map = {1: 'left', 0: 'right'}
 
+# method to get the english label of the input text
 def get_pol(content):
     tokenized = tokenizer.encode(content,
                                  max_length=context_length,
@@ -47,19 +50,23 @@ def get_pol(content):
                                  truncation = True
                             )
     inp = torch.tensor(tokenized).unsqueeze(0).to(device)
+    # pass through model
     logits = model(inp)
+    # extract the higher value probability and return the corresponding character label
     return reverse_label_map[int(torch.argmax(logits, dim=-1))]
 
 
-
+# make sure model is on gpu
 model.to(device)
+# read in the scraped subreddit data
 posts = pd.read_csv('./scrape/subreddits_cleaned.csv')
 
 
 print('starting to label')
 posts['content'] = posts['content'].fillna('').astype(str)
 model.eval()
-# loop is working, now hopefully i can to device this shit on the gpu
+# label each post using the get_pol method
 posts['affiliation'] = posts['content'].map(get_pol)
 
+# save resulting dataframe to csv
 posts.to_csv('./annotated_subreddit/posts_w_pol.csv')
